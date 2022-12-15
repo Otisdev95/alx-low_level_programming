cda40db932ec7217ef868296178f8b6ce87b686e
Otis <olaliotis95@gmail.com>
2022-12-14 19:31:47 -0800

    There are only 3 colors, 10 digits, and 7 notes; it's what we do with them that's important

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+void print_to_98(int n);
+int add(int, int);
+void times_table(void);
+void jack_bauer(void);
+int print_last_digit(int);
 int _abs(int);
 int print_sign(int c);
 int _isalpha(int c);
