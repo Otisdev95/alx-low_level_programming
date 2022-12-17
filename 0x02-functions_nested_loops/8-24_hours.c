aeaa7863753e1a46a052481fcbd99b5499466ecc
Otis <olaliotis95@gmail.com>
2022-12-17 09:36:40 -0800

    jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,41 +7,18 @@
 
 void jack_bauer(void)
 {
-	int h1;
-	int h2;
-	int m1;
-	int m2;
-	int a = 9;
+	int hour, minute;
 
-	h2 = 0;
-
-	while (h2 <= 2)
-		if (h2 == 2)
-		{
-			/*Restrain to 23h, not 29*/
-			a = 3;
-		}
-	h1 = 0;
-
-	while (h1 <= a)
+	for (huor = 0; hour <= 23; hour++)
 	{
-		m2 = 0;
-		while (m2 <= 5)
+		for (minute = 0; minute <= 59; minute++)
 		{
-			m1 = 0;
-			while (m1 <= 9)
-			{
-				_putchar('0' + h2);
-				_putchar('0' + h1);
-				_putchar(':');
-				_putchar('0' + m2);
-				_putchar('0' + m1);
-				_putchar('\n');
-				m1++;
-			}
-			m2++;
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
 		}
-		h1++;
 	}
-	h2++;
 }
