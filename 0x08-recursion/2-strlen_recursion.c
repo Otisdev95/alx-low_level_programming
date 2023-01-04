1bb89af871a3a20043a76f49c9dbcafc3fbc2d99
Otis <olaliotis95@gmail.com>
2023-01-04 04:55:41 -0800

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,12 +9,13 @@
 
 int _strlen_recursion(char *s)
 {
-	int len = 0;
-
 	if (*s)
 	{
-		len++;
-		len += _strlen_recursion(s + 1);
+		return (1 + _strlen_recursion(s + 1));
+	}
+	else
+	{
+		return (0);
 	}
 
 }
