91f48f8e8f93ebff9a70539e115763241030fff9
Otis <olaliotis95@gmail.com>
2023-01-04 05:09:37 -0800

    3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -10,11 +10,9 @@
 
 int factorial(int n)
 {
-	int result = n;
-
 	if (n < 0)
 		return (-1);
-
-	else if (n >= 0 && n <= 1)
+	if (n == 0 || n == 1)
 		return (1);
+	return (n * factorial(n - 1));
 }
