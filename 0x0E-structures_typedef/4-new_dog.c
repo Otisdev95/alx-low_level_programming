db9a7517baa13d104130904e4424d42f88b1f70b
Otis <olaliotis95@gmail.com>
2023-01-17 06:37:55 -0800

    4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,54 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * new_dog - new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return: newdog
+ */
+
+dog_t *new_dog(char *name. float age, char *owner)
+{
+	int i, lenN, lenO;
+
+	struct dog *nn_dog = NULL;
+
+	lenN = 0;
+
+	while (name[lenN] != '\0')
+		lenN++;
+
+	lenO = 0;
+
+	while (name[lenO] != '\0')
+		lenO++;
+
+	n_dog = malloc(sizeof(struct dog));
+	if (n_dog == NULL)
+	{
+		free(n_dog);
+		return (NULL);
+	}
+
+	n_dog->name = malloc(lenN + 1);
+
+	if (n_dog->name == NULL)
+	{
+		free(n_dog->name);
+		free(n_dog->owner);
+		free(n_dog);
+		return (NULL);
+	}
+
+	for (i = 0; i <= lenN; i++)
+		n_dog->name[i] = name[i];
+
+	for (i = 0; i <= lenO; i++)
+		n_dog->owner[i] = owner[i];
+	n_dog->age = age;
+	return (n_dog);
+}
