6477f2919bf3ff1b818ec0251fc679b2323c0679
Otis <olaliotis95@gmail.com>
2023-01-17 16:57:08 -0800

    A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,20 +10,18 @@
  * Return: NULL if fail.
  */
 
-dog_t *new_dog(char *name. float age, char *owner)
+dog_t *new_dog(char *name, float age, char *owner)
 {
 	int i, lenN, lenO;
 
 	struct dog *n_dog = NULL;
 
 	lenN = 0;
-
 	while (name[lenN] != '\0')
 		lenN++;
 
 	lenO = 0;
-
-	while (name[lenO] != '\0')
+	while (owner[lenO] != '\0')
 		lenO++;
 
 	n_dog = malloc(sizeof(struct dog));
@@ -32,20 +30,22 @@ dog_t *new_dog(char *name. float age, char *owner)
 		free(n_dog);
 		return (NULL);
 	}
-
 	n_dog->name = malloc(lenN + 1);
-
 	if (n_dog->name == NULL)
 	{
 		free(n_dog->name);
+		free(n_dog);
+		return (NULL);
+	}
+	n_dog->owner = malloc(lenO + 1);
+	if (n_dog->owner == NULL)
+	{
 		free(n_dog->owner);
 		free(n_dog);
 		return (NULL);
 	}
-
 	for (i = 0; i <= lenN; i++)
 		n_dog->name[i] = name[i];
-
 	for (i = 0; i <= lenO; i++)
 		n_dog->owner[i] = owner[i];
 	n_dog->age = age;
