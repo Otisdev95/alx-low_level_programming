d735d200142cbc670c084b99ad0bd045ba369819
Otis <olaliotis95@gmail.com>
2023-01-17 17:13:56 -0800

    If you can have that in your life, things will not be too bad

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,26 +11,22 @@
 
 void print_dog(struct dog *d)
 {
-	if (d)
+	if (d == 0)
+	{
+		return;
+	}
+	else
 	{
 		if (d->name == NULL)
-		{
-			printf("Name: %s\n", d->name);
-		}
-		else
-		{
 			printf("Name: (nil)\n");
-		}
+		else
+			printf("Name: %s\n", d->name);
 
 		printf("Age: %f\n", d->age);
 
-		if (d->owner)
-		{
-			printf("Owner: %s\n", d->owner);
-		}
-		else
-		{
+		if (d->owner == NULL)
 			printf("Owner: (nil)\n");
-		}
+		else
+			printf("Owner: %s\n", d->owner);
 	}
 }
