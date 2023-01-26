59e0f9f2123fb9c0c0bf5d41fabe8a10f248973c
Otis <olaliotis95@gmail.com>
2023-01-26 15:53:34 -0800

    The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-main.c
+++ 0x12-singly_linked_lists/100-main.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 	printf("(A tortoise, having pretty good sense"
-		"of a hare's nature, challenges one to"
-		"a race.)\n");
+		" of a hare's nature, challenges one to"
+		" a race.)\n");
 	return (0);
 }
