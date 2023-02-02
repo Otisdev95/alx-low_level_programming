73264cf8aa6a8e8b6c67614bafdc470b9737b08c
Otis <olaliotis95@gmail.com>
2023-02-02 14:55:56 -0800

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -18,7 +18,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		dec_val = 1 * dec_val + (b[i] - '0');
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
 	return (dec_val);
 }
