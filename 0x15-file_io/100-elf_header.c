a48f4139d79cc8fdda78fd7d7405eed645aa14b4
Otis <olaliotis95@gmail.com>
2023-02-06 20:58:43 -0800

    elf

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -30,10 +30,10 @@ void check_elf(unsigned char *e_ident)
 
 	for (index = 0; index < 4; index++)
 	{
-		if (e_indent[index] != 127 &&
-			e_indent[index] != 'E' &&
-			e_indent[index] != 'L' &&
-			e_indent[index] != 'F')
+		if (e_ident[index] != 127 &&
+			e_ident[index] != 'E' &&
+			e_ident[index] != 'L' &&
+			e_ident[index] != 'F')
 		{
 			dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
 			exit(98);
