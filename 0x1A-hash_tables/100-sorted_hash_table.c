02eb63892a2733cbbba9a60bc72c7095dc708522
otis <olaliotis95@gmail.com>
2023-04-09 02:51:13 -0700

    100-sorted_hash_table.c

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -31,3 +31,34 @@ shash_table_t *shash_table_create(unsigned long int size)
 
 	return (stc);
 }
+
+/**
+ * shash_table_get - Function that retrieves the sorted value associated
+ *	with number.
+ * @ht: The hash table in focus.
+ * @key: The key in search
+ *
+ * Return: The value associated with the element.
+ *		Otherwise - NULL if key could not be found.
+ */
+
+char *shash_table_get(const shash_table_t *ht, const char *key)
+{
+	shash_node_t *node;
+	unsigned long int index;
+
+	if (!ht || !key || *key == '\0')
+		return (NULL);
+
+	index = key_index((const unsigned char *)key , ht->size);
+
+	if (index >= ht->size)
+		return (NULL);
+
+	node = ht->array[index];
+
+	while (node != NULL && strcmp(node->key, key) != 0)
+		node = node->next;
+
+	return ((!node) ? NULL : node->value);
+}
